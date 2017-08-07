c455a33f251b5bf84dee059c8874c630b47c5118
snamihira <snamihira@pasonatech.co.jp>
2017-08-07 06:06:53 +0000

    HTMLを出力する

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -3,5 +3,17 @@ class HeloController < ApplicationController
     def index
         render plain:"Hello, This is Rails sample page!"
     end
-    
+
+    def index
+        msg = '
+            <html>
+            <body>
+            <h1>Sample Page</h1>
+            <p>this is Rails sample page!</p>
+            </body>
+            </html>
+            '    
+        render html: msg.html_safe
+    end
+
 end
