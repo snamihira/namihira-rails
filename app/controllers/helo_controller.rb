0abfd670bdec52399612f7a065c51d960381231f
snamihira <snamihira@pasonatech.co.jp>
2017-08-07 06:54:45 +0000

    use ERB

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -1,19 +1,6 @@
 class HeloController < ApplicationController
 
     def index
-        render plain:"Hello, This is Rails sample page!"
-    end
-
-    def index
-        msg = '
-            <html>
-            <body>
-            <h1>Sample Page</h1>
-            <p>this is Rails sample page!</p>
-            </body>
-            </html>
-            '    
-        render html: msg.html_safe
     end
 
 end
