010061949caec92d04f9da2b51c39364efcb3c2c
snamihira <snamihira@pasonatech.co.jp>
2017-08-08 01:18:53 +0000

    選択リストを使ったフォーム送信

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -3,17 +3,15 @@ class HeloController < ApplicationController
     def index
         if request.post? then
             @title = 'Result'
-            if params['r1'] then
-                @msg = 'you selected: ' + params['r1']
+            if params['s1'] then
+                @msg = 'you selected: ' + params['s1']
             else
                 @msg = 'not selected...'
             end
-
         else
             @title = 'Index'
-            @msg = 'selected radio button...'
+            @msg = 'select List...'
         end
-
     end
 
 end
